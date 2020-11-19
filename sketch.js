6d6b83be32f9ba676a04faed90b2a538dba526cb
Ashvin-Sharma-maker <72135799+Ashvin-Sharma-maker@users.noreply.github.com>
2020-11-19 16:02:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var redbox1,redbox2,redbox3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	var options = {isStatic:true};
+	redbox1 = Bodies.rectangle(400,640,130,20,options);
+	redbox2 = Bodies.rectangle(280,610,20,130,options);
+	redbox3 = Bodies.rectangle(520,610,20,130,options);
+
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 World.add(world,redbox1);
+	 World.add(world,redbox2);
+	 World.add(world,redbox3);
+
+	Engine.run(engine);
+
+	packageSprite.y = packageBody.position.x
+	packageSprite.y = packageBody.position.y
+
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  fill("RED");
+ rect(redbox1.position.x,redbox1.position.y,220,20);
+ rect(redbox2.position.x,redbox2.position.y,20,80);
+ rect(redbox3.position.x,redbox3.position.y,20,80);
+}
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	// Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false);
+ }
+}
+
+
